f0b959bff17dc801ef9a3f6d28f2f69236e3b28b
Carlos Eduardo <kaduu5050@outlook.com>
2024-03-25 21:19:18 -0300

    Initital commit

---
+++ app/src/main/java/com/example/colabs/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.colabs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.colabs.ui.theme.ColabsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ColabsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(top = 64.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = "PV", style = MaterialTheme.typography.titleLarge)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ColabsTheme {
+        Greeting("Android")
+    }
+}
