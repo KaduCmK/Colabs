8ec0c513898bd9d8fe166e39866abc52dbc98c97
Carlos Eduardo <kaduu5050@outlook.com>
2024-03-28 16:45:56 -0300

    mudei melhor tipografia do nome

--- app/src/main/java/com/example/colabs/MainActivity.kt
+++ app/src/main/java/com/example/colabs/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.colabs.ui.theme.ColabsTheme
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +50,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(text = "Luan")
         Text(text = "Yuri")
         Text(text = "Amin", style = MaterialTheme.typography.titleSmall)
+
+        Text(text = LocalDateTime.now().toString())
     }
 }
 
